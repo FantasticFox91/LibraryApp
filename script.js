185d573f8ff8b6f0cbff2c42372c297aaf490e98
Grygorii Lysytsia <grygorii_lysytsia@hotmail.comasd>
2021-07-07 20:32:10 +0430

    Change CSS styles

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
+// Assign variables
 let myLibrary = [];
+const addBookBtn = document.getElementById("addBook");
+const bookStorage = document.getElementsByClassName("container")[0];
 
-function Book() {
+function createNewBookContainer (){
+  const bookDiv = document.createElement("div");
+  const book = document.createElement("p");
+  book.textContent = "This is new book!";
+  bookDiv.classList = "book-container";
+  bookDiv.appendChild(book);
+  bookStorage.appendChild(bookDiv)
+}
+
+
+function addNewBook () {
+  const bookDiv = document.createElement("div");
+  const para = document.createElement("p")
+  bookStorage.appendChild(bookDiv.appendChild(para))
+}
+
+addBookBtn.addEventListener("click", createNewBookContainer)
+
+
+
+function Book(name,author,pages) {
   // the constructor...
+  this.name = name,
+  this.author = author,
+  this.pages = pages
 }
 
 function addBookToLibrary() {
