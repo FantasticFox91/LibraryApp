7c63809a24cfc163afff06007e4f88ea77cea6b0
Grygorii Lysytsia <grygorii_lysytsia@hotmail.comasd>
2021-07-14 23:08:45 +0430

    Update css file and add form, add onclick functions for buttons

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
 // Assign variables
 let myLibrary = [];
-const addBookBtn = document.getElementById("addBook");
+
 const bookStorage = document.getElementsByClassName("container")[0];
-
-function createNewBookContainer (){
-  const bookDiv = document.createElement("div");
-  const book = document.createElement("p");
-  book.textContent = "This is new book!";
-  bookDiv.classList = "book-container";
-  bookDiv.appendChild(book);
-  bookStorage.appendChild(bookDiv)
-}
-
-
-function addNewBook () {
-  const bookDiv = document.createElement("div");
-  const para = document.createElement("p")
-  bookStorage.appendChild(bookDiv.appendChild(para))
-}
-
-addBookBtn.addEventListener("click", createNewBookContainer)
-
-
+const menuBtn = document.getElementById("menu");
+const bookNameInput = document.querySelector("#bname");
+const authorInput = document.querySelector("#author");
+const addBookBtn = document.querySelector("#addBook");
+const startContainer = document.querySelector(".book-container-start");
+const addBookTo = document.querySelector("#addBookToContainer");
+const addBookForm = document.querySelector(".formBook");
+const closeBtn = document.querySelector('.closeBtn');
+const formBtn = document.querySelector('#addBookBtn');
+const pages = document.querySelector('#pages');
+let author = "";
+let bookName = "";
 
 function Book(name,author,pages) {
   // the constructor...
@@ -33,3 +25,76 @@ function Book(name,author,pages) {
 function addBookToLibrary() {
   // do stuff here
 }
+
+window.onclick = function (event) {
+  if (event.target.className === "formBook") {
+    event.target.classList.toggle('hide')
+    }
+}
+
+
+
+// addBookTo.onclick = function(){
+//   startContainer.classList.add("hide")
+//   let newDiv = document.createElement("div");
+//   let bookNameOutput = document.createElement("p");
+//   bookNameOutput.textContent = bookName;
+//   let bookAuthor = document.createElement("p");
+//   bookAuthor.textContent = author;
+//   newDiv.appendChild(bookNameOutput);
+//   newDiv.appendChild(bookAuthor);
+//   newDiv.classList.add("book-container");
+//   bookStorage.appendChild(newDiv)
+// }
+
+addBookBtn.onclick = function(){
+  addBookForm.classList.toggle("hide")
+}
+
+closeBtn.onclick = function(){
+  addBookForm.classList.toggle("hide")
+};
+
+function clearInputs(){
+  bookNameInput.value = "";
+  authorInput.value = "";
+  pages.value = "";
+}
+
+function updateBookContainers () {
+  bookStorage.firstElementChild.remove();
+  let newDiv = document.createElement("div");
+  let bookNameOutput = document.createElement("p");
+  bookNameOutput.textContent = myLibrary[0].bookName;
+  let bookAuthor = document.createElement("p");
+  bookAuthor.textContent = myLibrary[0].author;
+  newDiv.appendChild(bookNameOutput);
+  newDiv.appendChild(bookAuthor);
+  newDiv.classList.add("book-container");
+  bookStorage.appendChild(newDiv)
+};
+
+getBookinfo = () =>{
+  let bName = document.querySelector('#bname').value;
+  let aName = document.querySelector('#author').value;
+  let pages = document.querySelector('#pages').value;
+  let genre = document.querySelector('#genres').value;
+  let readYesNo = document.querySelector('#read').value
+  let a = {
+    bookName : bName,
+    author: aName,
+    pages: pages,
+    genre : genre,
+    read: readYesNo
+  }
+  myLibrary.push(a)
+  clearInputs()
+  addBookForm.classList.toggle("hide");
+  updateBookContainers();
+};
+
+
+
+// formBtn.onclick = function () {
+//   getBookinfo();
+// } 
