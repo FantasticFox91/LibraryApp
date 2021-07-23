3cc0fd2dfff7e2e1ece75f526e6024e617270bd0
Grygorii Lysytsia <grygorii_lysytsia@hotmail.comasd>
2021-07-23 17:32:02 +0430

    Refactoring and self code review

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ const menuBtn = document.querySelector("#menu");
 const bookNameInput = document.querySelector("#bname");
 const authorInput = document.querySelector("#author");
 const addBookBtn = document.querySelector("#addBook");
-const startContainer = document.querySelector(".book-container-start");
-const addBookTo = document.querySelector("#addBookToContainer");
 const addBookForm = document.querySelector(".formBook");
 const closeBtn = document.querySelector('.closeBtn');
 const formBtn = document.querySelector('#addBookBtn');
@@ -19,14 +17,13 @@ let bookCounter = document.querySelector("#booksCounter")
 let bookReadCounter = document.querySelector("#readCounter");
 let bookNotReadCounter = document.querySelector("#notReadCounter");
 let pagesRead = document.querySelector('#pagesRead');
-let result = 0;
 
-let author = "";
-let bookName = "";
+let index = 0;
 let id = 0;
-let checker = 0;
+
 
 //Create constructor for future book objects
+
 function Book(id,name,author,pages,genre,read) {
   this.id = id,
   this.name = name,
@@ -37,75 +34,46 @@ function Book(id,name,author,pages,genre,read) {
   this.read = read
 }
 
-//Close add book form if clicked outside the from
-window.onclick = function (event) {
-  if (event.target.className === "formBook") {
-    event.target.classList.toggle('hide')
-    }
-}
+//Functions
 
-//Show the add book form if yoy click Add book button
-addBookBtn.onclick = function(){
-  addBookForm.classList.toggle("hide")
-}
-
-//Hide add book form after close button clicked
-closeBtn.onclick = function(){
-  addBookForm.classList.toggle("hide")
-};
+//Toggle .hide class for add book form
+let hideAddBookForm = () => addBookForm.classList.toggle("hide");
 
 //Clear inputs from user's information
-function clearInputs(){
+let clearInputs = () =>{
   bookNameInput.value = "";
   authorInput.value = "";
   pages.value = "";
 }
 
 // Update local storage 
-function transferArrayToLocalStorage(){
-  localStorage.setItem("books",JSON.stringify(myLibrary));
-  let myLibraryLocalStorage
-  myLibraryLocalStorage = JSON.parse(localStorage.getItem("books"));
-}
+let updateLocalStorage = () =>localStorage.setItem("books",JSON.stringify(myLibrary));
 
 //Clear main container
-function removeAllChildNodes(parent) {
+let removeAllChildNodes = (parent) => {
   while (parent.firstChild) {
-      parent.removeChild(parent.firstChild);
+    parent.removeChild(parent.firstChild);
   }
 }
 
 //Delete clicked book from array
-function deleteItemFromArray(event){
+let deleteItemFromArray = (event) => {
   event.path[2].remove();
   let indexItem = event.path[1].dataset.id
-  
-  updateCounters();
-}
-
-//Delete clicked book from local storage
-function deleteItemFromLocal(event){
-  event.path[2].remove();
-  let myLibraryLocalStorage = JSON.parse(localStorage.getItem("books"))
-  console.log(1,myLibraryLocalStorage)
-  let indexItem = event.path[1].dataset.id
-  myLibrary.splice(indexItem,1)
-  myLibraryLocalStorage.splice(indexItem,1)
-  console.log(2,myLibraryLocalStorage)
-  transferArrayToLocalStorage()
   updateCountersLocal();
 }
 
-//Update counter in user's infro container from array
-function updateCounters (){
-  bookCounter.textContent = myLibrary.length;
-  bookReadCounter.textContent = myLibrary.filter((el)=> el.read === "read").length;
-  bookNotReadCounter.textContent = myLibrary.filter((el)=> el.read === "not read").length;
-  pagesRead.textContent = myLibrary.map(el=> el.pages).reduce((a,b)=>Number(a)+Number(b),0)
+//Delete clicked book from local storage
+let deleteItemFromLocal = (event) =>{
+  event.path[2].remove();
+  let indexItem = event.path[1].dataset.id
+  myLibrary.splice(indexItem,1)
+  updateLocalStorage();
+  updateCountersLocal();
 }
 
 //Update counter in user's infro container from local storage
-function updateCountersLocal (){
+let updateCountersLocal = () => {
   let myLibraryLocalStorage = JSON.parse(localStorage.getItem("books"))
   bookCounter.textContent = myLibraryLocalStorage.length;
   bookReadCounter.textContent = myLibraryLocalStorage.filter((el)=> el.read === "read").length;
@@ -113,168 +81,119 @@ function updateCountersLocal (){
   pagesRead.textContent = myLibraryLocalStorage.map(el=> el.pages).reduce((a,b)=>Number(a)+Number(b),0)
 }
 
+let createBookContainer = (el) =>{
+  let newDiv = document.createElement("div");
+  let bookNameOutput = document.createElement("h2");
+  let bookAuthor = document.createElement("p");
+  let deleteBtn = document.createElement("button");
+  let readBtn = document.createElement("button");
+  let divBtn = document.createElement("div");
 
+  divBtn.classList.add("book-container-btns");
+  readBtn.classList.add("readBtn");
+  deleteBtn.classList.add("deleteBtn");
+  deleteBtn.setAttribute('data-id', `${id}`)
+  if(el.read === "read"){
+    newDiv.classList.add('book-container');
+    }else{
+    newDiv.classList.add('book-container');
+    newDiv.classList.add('notRead');
+  }
 
-//Update main container according to array
-function updateBookContainers () {
-     removeAllChildNodes(bookStorage);
-     id = 0; 
-      myLibrary.map((el)=>{
-        let newDiv = document.createElement("div");
-        let bookNameOutput = document.createElement("h2");
-        let bookAuthor = document.createElement("p");
-        let deleteBtn = document.createElement("button");
-        let readBtn = document.createElement("button");
-        let divBtn = document.createElement("div");
+  newDiv.setAttribute('data-id', `${id}`)
 
-        divBtn.classList.add("book-container-btns");
-        readBtn.classList.add("readBtn");
-        deleteBtn.classList.add("deleteBtn");
-        deleteBtn.setAttribute('data-id', `${id}`)
-        if(el.read === "read"){
-          newDiv.classList.add('book-container');
-        }else{
-          newDiv.classList.add('book-container');
-          newDiv.classList.add('notRead');
-        }
-        
-        newDiv.setAttribute('data-id', `${id}`)
+  bookNameOutput.textContent = el.name;
+  readBtn.textContent = "Δ";
+  deleteBtn.textContent = "╳";
+  bookAuthor.textContent =`This book is written by ${el.author} with ${el.pages} pages. It is written in ${el.genre} genre and you ${el.read === "read" ? 'read ' : "haven't read "} it`;
+  divBtn.appendChild(readBtn);
+  divBtn.appendChild(deleteBtn);
+  newDiv.appendChild(bookNameOutput);
+  newDiv.appendChild(bookAuthor);
+  newDiv.appendChild(divBtn);
+  bookStorage.appendChild(newDiv)
+       
+  id++
+  updateCountersLocal();
+}
 
-        bookNameOutput.textContent = el.name;
-        readBtn.textContent = "Δ";
-        deleteBtn.textContent = "╳";
-        bookAuthor.textContent =`This book is written by ${el.author} with ${el.pages} pages. It is written in ${el.genre} genre and you ${el.read === "read" ? 'read ' : "haven't read "} it`;
-        
-
-        divBtn.appendChild(readBtn);
-        divBtn.appendChild(deleteBtn);
-        newDiv.appendChild(bookNameOutput);
-        newDiv.appendChild(bookAuthor);
-        newDiv.appendChild(divBtn);
-        bookStorage.appendChild(newDiv)
-        
-        id++
-        updateCounters();
-        transferArrayToLocalStorage()
-      }
-      )
-     
-      
-    deleteBtns = document.querySelectorAll('.deleteBtn');
-    deleteBtns.forEach((el)=>el.onclick = deleteItemFromArray);
-    const changeReadStatusBtn = document.querySelectorAll('.readBtn');
-    changeReadStatusBtn.forEach((el)=> el.onclick = function(event){
-      let indexItem = event.path[2].dataset.id;
-      if(myLibrary[indexItem].read === "read"){
-        myLibrary[indexItem].read = "not read";
-        document.getElementsByClassName('book-container')[indexItem].classList.add('notRead');
-        document.querySelectorAll('.book-container p')[indexItem].textContent = `This book is written by ${myLibrary[indexItem].author} with ${myLibrary[indexItem].pages} pages. It is written in ${myLibrary[indexItem].genre} genre and you ${myLibrary[indexItem].read === "read" ? 'read ' : "haven't read "} it`
-        }else{
-        myLibrary[indexItem].read = "read"
-        document.getElementsByClassName('book-container')[indexItem].classList.remove('notRead');
-        document.querySelectorAll('.book-container p')[indexItem].textContent = `This book is written by ${myLibrary[indexItem].author} with ${myLibrary[indexItem].pages} pages. It is written in ${myLibrary[indexItem].genre} genre and you ${myLibrary[indexItem].read === "read" ? 'read ' : "haven't read "} it`
-      }
-      
-    })
-    
+let changeReadStatus = (event) =>{
+  let indexItem = event.path[2].dataset.id;
+  if(myLibrary[indexItem].read === "read"){
+    myLibrary[indexItem].read = "not read";
+    localStorage.setItem("books",JSON.stringify(myLibrary));
+    document.getElementsByClassName('book-container')[indexItem].classList.add('notRead');
+    document.querySelectorAll('.book-container p')[indexItem].textContent = `This book is written by ${myLibrary[indexItem].author} with ${myLibrary[indexItem].pages} pages. It is written in ${myLibrary[indexItem].genre} genre and you ${myLibrary[indexItem].read === "read" ? 'read ' : "haven't read "} it`;
+    myLibrary = JSON.parse(localStorage.getItem("books"))
+    updateCountersLocal(); 
+    }else{
+      myLibrary[indexItem].read = "read";
+      localStorage.setItem("books",JSON.stringify(myLibrary));
+      document.getElementsByClassName('book-container')[indexItem].classList.remove('notRead');
+      document.querySelectorAll('.book-container p')[indexItem].textContent = `This book is written by ${myLibrary[indexItem].author} with ${myLibrary[indexItem].pages} pages. It is written in ${myLibrary[indexItem].genre} genre and you ${myLibrary[indexItem].read === "read" ? 'read ' : "haven't read "} it`
+      myLibrary = JSON.parse(localStorage.getItem("books"))
+      updateCountersLocal(); 
   }
   
-  //Update main container using local storage 
-  function updateBookContainersLocal () {
+} 
+
+//Update main container using local storage 
+
+function updateBookContainersLocal () {
     removeAllChildNodes(bookStorage);
     id = 0;
-    let myLibraryLocalStorage = JSON.parse(localStorage.getItem("books"))
-    if(myLibraryLocalStorage.length > 0){
-     myLibraryLocalStorage.map((el)=>{
-       let newDiv = document.createElement("div");
-       let bookNameOutput = document.createElement("h2");
-       let bookAuthor = document.createElement("p");
-       let deleteBtn = document.createElement("button");
-       let readBtn = document.createElement("button");
-       let divBtn = document.createElement("div");
+    myLibrary = JSON.parse(localStorage.getItem("books"))
 
-       divBtn.classList.add("book-container-btns");
-       readBtn.classList.add("readBtn");
-       deleteBtn.classList.add("deleteBtn");
-       deleteBtn.setAttribute('data-id', `${id}`)
-       if(el.read === "read"){
-         newDiv.classList.add('book-container');
-       }else{
-         newDiv.classList.add('book-container');
-         newDiv.classList.add('notRead');
-       }
-       
-       newDiv.setAttribute('data-id', `${id}`)
+    if(myLibrary.length > 0){
+     myLibrary.map((el)=>createBookContainer(el))
+    }
 
-       bookNameOutput.textContent = el.name;
-       readBtn.textContent = "Δ";
-       deleteBtn.textContent = "╳";
-       bookAuthor.textContent =`This book is written by ${el.author} with ${el.pages} pages. It is written in ${el.genre} genre and you ${el.read === "read" ? 'read ' : "haven't read "} it`;
-       
-
-       divBtn.appendChild(readBtn);
-       divBtn.appendChild(deleteBtn);
-       newDiv.appendChild(bookNameOutput);
-       newDiv.appendChild(bookAuthor);
-       newDiv.appendChild(divBtn);
-       bookStorage.appendChild(newDiv)
-       
-       id++
-       updateCountersLocal();
-     }
-     )
-     deleteBtns = document.querySelectorAll('.deleteBtn');
+    deleteBtns = document.querySelectorAll('.deleteBtn');
     deleteBtns.forEach((el)=>el.onclick = deleteItemFromLocal);
+
     const changeReadStatusBtn = document.querySelectorAll('.readBtn');
-    changeReadStatusBtn.forEach((el)=> el.onclick = function(event){
-      let indexItem = event.path[2].dataset.id;
-      if(myLibraryLocalStorage[indexItem].read === "read"){
-        myLibraryLocalStorage[indexItem].read = "not read";
-        localStorage.setItem("books",JSON.stringify(myLibraryLocalStorage));
-        document.getElementsByClassName('book-container')[indexItem].classList.add('notRead');
-        document.querySelectorAll('.book-container p')[indexItem].textContent = `This book is written by ${myLibraryLocalStorage[indexItem].author} with ${myLibraryLocalStorage[indexItem].pages} pages. It is written in ${myLibraryLocalStorage[indexItem].genre} genre and you ${myLibraryLocalStorage[indexItem].read === "read" ? 'read ' : "haven't read "} it`;
-        myLibrary = JSON.parse(localStorage.getItem("books"))
-        
-        }else{
-        myLibraryLocalStorage[indexItem].read = "read";
-        localStorage.setItem("books",JSON.stringify(myLibraryLocalStorage));
-        document.getElementsByClassName('book-container')[indexItem].classList.remove('notRead');
-        document.querySelectorAll('.book-container p')[indexItem].textContent = `This book is written by ${myLibraryLocalStorage[indexItem].author} with ${myLibraryLocalStorage[indexItem].pages} pages. It is written in ${myLibraryLocalStorage[indexItem].genre} genre and you ${myLibraryLocalStorage[indexItem].read === "read" ? 'read ' : "haven't read "} it`
-        myLibrary = JSON.parse(localStorage.getItem("books"))
-       
-      }
-    })} 
-  }
- let i = 0; 
+    changeReadStatusBtn.forEach((el)=> el.onclick = changeReadStatus)
+  
+  } 
 
-
- //Recieve information from add book form
+  //Recieve information from add book form
 function getBookinfo (e) {
   e.preventDefault();
-  if(bookNameInput.value === "" || authorInput.value === "" || pages.value === ""){
-    alert("Please fill required fields. Check if you wrote them in English")  
-  }else{
   let bName = document.querySelector('#bname').value;
   let aName = document.querySelector('#author').value;
   let pages = document.querySelector('#pages').value;
   let genre = document.querySelector('#genres').value;
   let read = document.querySelector('#readStatusValue').value
   let a = new Book (id,bName,aName, pages, genre, read)
-   myLibrary.push(a)
-   localStorage.setItem(`book${i}`, JSON.stringify(a))
-   i++
+  myLibrary.push(a)
+  localStorage.setItem(`book${index}`, JSON.stringify(a))
+  updateLocalStorage()
+  index++
   clearInputs()
-  addBookForm.classList.toggle("hide");
-  updateBookContainers();
-  }
-  
+  hideAddBookForm()
+  updateBookContainersLocal();
 }
   
+
+
+//Event listeners
+
+//Close add book form if clicked outside the from
+window.onclick = (event) => (event.target.className === "formBook") ? event.target.classList.toggle('hide') : "";
+
+//Show the add book form if yoy click Add book button
+addBookBtn.onclick = () => hideAddBookForm();
+
+//Hide add book form after close button clicked
+closeBtn.onclick = () => hideAddBookForm();
+
 //Add event listener after form submit
 form.addEventListener('submit', getBookinfo)
 
 //Add functionality to menu button(only if screen size less than 810px)
-menuBtn.onclick = function(){addBookForm.classList.toggle("hide")};
+menuBtn.onclick = () => hideAddBookForm();
+
+//On load
 
 //Initiante retrieve infromation from local storage on refresh or reopen the page
 updateBookContainersLocal();
